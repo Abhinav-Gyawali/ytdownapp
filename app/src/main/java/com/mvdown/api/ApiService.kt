49754abf6c58305aa8a21eea747d09504c73673d
Abhinav-Gyawali <95914404+Abhinav-Gyawali@users.noreply.github.com>
2025-10-11 12:30:10 +0545

    Update ApiService.kt

--- app/src/main/java/com/mvdown/api/ApiService.kt
+++ app/src/main/java/com/mvdown/api/ApiService.kt
@@ -1,42 +1,35 @@
 package com.mvdown.api
 
-
-
-import com.mvdown.Format
-
+import com.mvdown.models.DownloadRequest
+import com.mvdown.models.DownloadResponse
+import com.mvdown.models.FileInfo
+import com.mvdown.models.FormatRequest
+import com.mvdown.models.FormatResponse
+import okhttp3.ResponseBody
 import retrofit2.Response
-
-import retrofit2.http.GETimport com.mvdown.Formatimport com.mvdown.models.Format
-
-import retrofit2.http.Query
-
-import retrofit2.Responseimport com.mvdown.models.MediaInfo
+import retrofit2.http.*
 
 interface ApiService {
-
-    @GET("/formats")import retrofit2.http.GETimport retrofit2.Response
-
-    suspend fun getFormats(@Query("url") url: String): Response<List<Format>>
-
-}import retrofit2.http.Queryimport retrofit2.http.GET
-
-import retrofit2.http.POST
-
-interface ApiService {import retrofit2.http.Query
-
-    @GET("/formats")
-
-    suspend fun getFormats(@Query("url") url: String): Response<List<Format>>interface ApiService {
-
-}    @GET("info")
-    suspend fun getMediaInfo(@Query("url") url: String): Response<MediaInfo>
     
-    @GET("formats")
-    suspend fun getFormats(@Query("url") url: String): Response<List<Format>>
+    @POST("api/formats")
+    suspend fun getFormats(@Body request: FormatRequest): Response<FormatResponse>
     
-    @POST("download")
-    suspend fun getDownloadLink(
-        @Query("url") url: String,
-        @Query("format") format: String
-    ): Response<String>
+    @POST("api/download")
+    suspend fun initiateDownload(@Body request: DownloadRequest): Response<DownloadResponse>
+    
+    @GET("files")
+    suspend fun getFiles(): Response<List<FileInfo>>
+    
+    @DELETE("api/files/{filename}")
+    suspend fun deleteFile(@Path("filename") filename: String): Response<Unit>
+    
+    @Streaming
+    @GET("downloads/{filename}")
+    suspend fun downloadFile(
+        @Path("filename") filename: String,
+        @Header("Range") range: String? = null
+    ): Response<ResponseBody>
+    
+    @GET("health")
+    suspend fun healthCheck(): Response<Map<String, String>>
 }
